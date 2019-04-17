0050a5a0ac48ca34e73466a060d692529eaf6c6a
Alex Kouznetsov <akouz@users.noreply.github.com>
2019-04-17 13:56:57 +0930

    Add files via upload

---
+++ Devices/06_MOSFET4/Sketches/White_LED_strip/HBmqtt.h
@@ -0,0 +1,65 @@
+/*
+ * file     HBmqtt.h 
+ * Target   Arduino
+
+ * (c) 2019 Alex Kouznetsov,  https://github.com/akouz/hbus
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef __HB_MQTT_H
+#define __HB_MQTT_H
+
+//##############################################################################
+// Inc                                              
+//##############################################################################
+
+#include  "HBcommon.h"
+
+
+//##############################################################################
+// Class
+//##############################################################################
+
+class HB_mqtt{
+    public:
+                HB_mqtt(void);
+    hb_msg_t    mqmsg;
+//    uint        topic[MAX_TOPIC];           // list of topics  
+    float       value[MAX_TOPIC];           // topic values
+    uchar       valid[MAX_TOPIC];           // indicate valid value
+    void        set_descriptor(uint* descr);
+    uint        get_topic(uchar tpc_i);     // get topic value               
+    char        rd_msg(hb_msg_t* msg);
+    uchar       make_msg(uchar topic_i);    
+    
+    private:
+    uint*       descriptor;                 // list of topics 
+    char        is_topic(uint tpc);
+    uint        MsgID; 
+    ulong       MsgID_cnt;                  // count all received MQTT messages 
+    uint        MsgID_err_cnt;
+    void        get_MsgID(uint msg_id); 
+};    
+
+extern HB_mqtt HBmqtt;
+
+
+
+#endif /* __HB_MQTT_H */
---
+++ Devices/06_MOSFET4/Sketches/White_LED_strip/README.md
@@ -0,0 +1,15 @@
+ # White LED strip
+ 
+This sketch combines functions of light sensor and movement detector:
+  * In the evening it set LED strip on for 4 hours at half brightness. PWM frequency 125 Hz.
+  * If a movement detected while it is dark then LED strip is set to full brightness for 1 min; it is retriggered by every detected movement
+  
+Apart from "visual" functions, sketch sends the following report to HBus:
+  * topic 1200 - relative light level, less than 0.9 is dark, greater than 1.0 is bright, value greater than 1.1 reloads the 4-hour timer
+  * topic 1300 - movement detector, value reflects number of seconds before timer will be set off
+  * topic 2000 - LED strip light level,  value in the range 0...1.0 reflect PWM output to the LED strip
+  * topic 500 - integrity check: set to 1.0 if there is no MOSFET voltage detected, or to 0 if MOSFET voltage is OK
+
+Topics reported promply on change, as well as merely updated every 10 min. 
+
+By setting topic 2000 value, it is possdible to control LED strip remotely.
